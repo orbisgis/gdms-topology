c5988ee718e07df7310ad15a3728bedc467f313c
ebocher <ebocher@irstv>
2011-11-29 14:18:23 +0000

    GDMS-Topology update header

--- src/main/java/org/gdms/gdmstopology/function/ST_FindReachableEdges.java
+++ src/main/java/org/gdms/gdmstopology/function/ST_FindReachableEdges.java
@@ -1,3 +1,30 @@
+/**
+ * GDMS-Topology  is a library dedicated to graph analysis. It's based on the JGraphT available at
+ * http://www.jgrapht.org/. It ables to compute and process large graphs using spatial
+ * and alphanumeric indexes.
+ * This version is developed at French IRSTV institut as part of the
+ * EvalPDU project, funded by the French Agence Nationale de la Recherche
+ * (ANR) under contract ANR-08-VILL-0005-01 and GEBD project
+ * funded by the French Ministery of Ecology and Sustainable Development .
+ *
+ * GDMS-Topology  is distributed under GPL 3 license. It is produced by the "Atelier SIG" team of
+ * the IRSTV Institute <http://www.irstv.cnrs.fr/> CNRS FR 2488.
+ *
+ * GDMS-Topology is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * GDMS-Topology is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * GDMS-Topology. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more information, please consult: <http://trac.orbisgis.org/>
+ * or contact directly:
+ * info_at_ orbisgis.org
+ */
 package org.gdms.gdmstopology.function;
 
 import com.vividsolutions.jts.geom.Geometry;
@@ -29,7 +56,8 @@ import org.jgrapht.graph.EdgeReversedGraph;
 
 /**
  *
- * @author ebocher
+ * @author ebocher IRSTV FR CNRS 2488
+ *
  */
 public class ST_FindReachableEdges extends AbstractTableFunction {
 
@@ -61,7 +89,7 @@ public class ST_FindReachableEdges extends AbstractTableFunction {
 
         @Override
         public void workFinished() throws DriverException {
-                if(diskBufferDriver!=null) {
+                if (diskBufferDriver != null) {
                         diskBufferDriver.stop();
                 }
         }
--- src/main/java/org/gdms/gdmstopology/function/ST_ShortestPathLength.java
+++ src/main/java/org/gdms/gdmstopology/function/ST_ShortestPathLength.java
@@ -1,3 +1,31 @@
+/**
+ * GDMS-Topology  is a library dedicated to graph analysis. It's based on the JGraphT available at
+ * http://www.jgrapht.org/. It ables to compute and process large graphs using spatial
+ * and alphanumeric indexes.
+ * This version is developed at French IRSTV institut as part of the
+ * EvalPDU project, funded by the French Agence Nationale de la Recherche
+ * (ANR) under contract ANR-08-VILL-0005-01 and GEBD project
+ * funded by the French Ministery of Ecology and Sustainable Development .
+ *
+ * GDMS-Topology  is distributed under GPL 3 license. It is produced by the "Atelier SIG" team of
+ * the IRSTV Institute <http://www.irstv.cnrs.fr/> CNRS FR 2488.
+ *
+ * GDMS-Topology is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * GDMS-Topology is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * GDMS-Topology. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more information, please consult: <http://trac.orbisgis.org/>
+ * or contact directly:
+ * info_at_ orbisgis.org
+ */
+
 package org.gdms.gdmstopology.function;
 
 import org.gdms.data.DataSourceFactory;
@@ -73,12 +101,13 @@ public class ST_ShortestPathLength extends AbstractTableFunction {
 
         @Override
         public String getDescription() {
-                return "Return the shortest path length beetwen one vertex to all other based on a directed graph. True if the path is computed using an undirected graph.";
+                return "Return the shortest path length beetwen one vertex to all other based on a directed graph. True if the path is computed using an undirected graph."
+                        + "The last boolean argument is used to reverse or the edges.";
         }
 
         @Override
         public String getSqlOrder() {
-                return "SELECT * from ST_ShortestPathLength(table, 12[,true]) );";
+                return "SELECT * from ST_ShortestPathLength(table, 12[,true, false]) );";
         }
 
         @Override
