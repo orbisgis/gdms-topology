caf819ef797c221d604f8bc3add1a76b499560c1
Antoine Gourlay <antoine@gourlay.fr>
2012-06-18 09:11:40 +0200

    update license headers to new legal format.

--- src/main/java/org/gdms/gdmstopology/demo/DemoGraph.java
+++ src/main/java/org/gdms/gdmstopology/demo/DemoGraph.java
@@ -1,3 +1,36 @@
+/**
+ * GDMS-Topology  is a library dedicated to graph analysis. It is based on the JGraphT
+ * library available at <http://www.jgrapht.org/>. It enables computing and processing
+ * large graphs using spatial and alphanumeric indexes.
+ *
+ * This version is developed at French IRSTV institut as part of the
+ * EvalPDU project, funded by the French Agence Nationale de la Recherche
+ * (ANR) under contract ANR-08-VILL-0005-01 and GEBD project
+ * funded by the French Ministery of Ecology and Sustainable Development.
+ *
+ * GDMS-Topology  is distributed under GPL 3 license. It is produced by the "Atelier SIG"
+ * team of the IRSTV Institute <http://www.irstv.fr/> CNRS FR 2488.
+ *
+ * Copyright (C) 2009-2012 IRSTV (FR CNRS 2488)
+ *
+ * Contributors:
+ *  - 2009-2012: Erwan Bocher, Alexis Guéganno
+ *
+ * GDMS-Topology is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * GDMS-Topology is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * GDMS-Topology. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more information, please consult: <http://trac.orbisgis.org/>
+ * or contact directly:
+ * info_at_ orbisgis.org
+ */
 package org.gdms.gdmstopology.demo;
 
 import java.io.File;
--- src/main/java/org/gdms/gdmstopology/model/GraphEdge.java
+++ src/main/java/org/gdms/gdmstopology/model/GraphEdge.java
@@ -1,14 +1,20 @@
 /**
- * GDMS-Topology  is a library dedicated to graph analysis. It's based on the JGraphT available at
- * http://www.jgrapht.org/. It ables to compute and process large graphs using spatial
- * and alphanumeric indexes.
+ * GDMS-Topology  is a library dedicated to graph analysis. It is based on the JGraphT
+ * library available at <http://www.jgrapht.org/>. It enables computing and processing
+ * large graphs using spatial and alphanumeric indexes.
+ *
  * This version is developed at French IRSTV institut as part of the
  * EvalPDU project, funded by the French Agence Nationale de la Recherche
  * (ANR) under contract ANR-08-VILL-0005-01 and GEBD project
- * funded by the French Ministery of Ecology and Sustainable Development .
+ * funded by the French Ministery of Ecology and Sustainable Development.
  *
- * GDMS-Topology  is distributed under GPL 3 license. It is produced by the "Atelier SIG" team of
- * the IRSTV Institute <http://www.irstv.cnrs.fr/> CNRS FR 2488.
+ * GDMS-Topology  is distributed under GPL 3 license. It is produced by the "Atelier SIG"
+ * team of the IRSTV Institute <http://www.irstv.fr/> CNRS FR 2488.
+ *
+ * Copyright (C) 2009-2012 IRSTV (FR CNRS 2488)
+ *
+ * Contributors:
+ *  - 2009-2012: Erwan Bocher, Alexis Guéganno
  *
  * GDMS-Topology is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
@@ -25,7 +31,6 @@
  * or contact directly:
  * info_at_ orbisgis.org
  */
-
 package org.gdms.gdmstopology.model;
 
 import org.jgrapht.graph.DefaultWeightedEdge;
