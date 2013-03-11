58a67e7c17a7892e3cce5e6e2f8d35bf3f053b2a
Adam Gouge <adam.gouge@ec-nantes.fr>
2013-03-11 15:38:02 +0100

    Restor Erwan's version of ST_ShortestPath.
    I had written a different version of this function that didn't quite
    work correctly. I would like to refactor this function later to make it
    easier to read and debug.

--- src/main/java/org/gdms/gdmstopology/function/ST_ShortestPath.java
+++ src/main/java/org/gdms/gdmstopology/function/ST_ShortestPath.java
@@ -33,17 +33,14 @@
 package org.gdms.gdmstopology.function;
 
 import org.gdms.data.DataSourceFactory;
-import org.gdms.data.indexes.IndexException;
 import org.gdms.data.schema.Metadata;
 import org.gdms.data.values.Value;
 import org.gdms.driver.DataSet;
 import org.gdms.driver.DiskBufferDriver;
 import org.gdms.driver.DriverException;
-import org.gdms.gdmstopology.graphcreator.GraphCreator;
-import org.gdms.gdmstopology.model.GraphException;
+import org.gdms.gdmstopology.model.GraphMetadataFactory;
 import org.gdms.gdmstopology.model.GraphSchema;
-import org.gdms.gdmstopology.parse.GraphFunctionParser;
-import org.gdms.gdmstopology.process.GraphPathCalculator;
+import org.gdms.gdmstopology.process.GraphPath;
 import org.gdms.sql.function.FunctionException;
 import org.gdms.sql.function.FunctionSignature;
 import org.gdms.sql.function.ScalarArgument;
@@ -57,17 +54,9 @@ import org.orbisgis.progress.ProgressMonitor;
  * Calculates the shortest path between two vertices of a graph using Dijkstra's
  * algorithm.
  *
- * <p> Example usage: <center> {@code SELECT * from
- * ST_ShortestPath(
- * input_table,
- * source_vertex,
- * target_vertex,
- * 'weights_column'[,orientation]);} </center> or: <center> {@code SELECT * from
- * ST_ShortestPath(
- * input_table,
- * source_vertex,
- * target_vertex,
- * 1[,orientation]);} </center>
+ * <p> Example usage: <center>
+ * {@code SELECT * from  ST_ShortestPath(input_table, source_vertex, target_vertex, 'weights_column'[,orientation]);}
+ * </center>
  *
  * <p> Required parameters: <ul> <li> {@code input_table} - the input table.
  * Specifically, this is the {@code output_table_prefix.edges} table produced by
@@ -76,8 +65,7 @@ import org.orbisgis.progress.ProgressMonitor;
  * {@code source_vertex} - an integer specifying the source vertex. <li>
  * {@code target_vertex} - an integer specifying the target vertex. <li>
  * {@code 'weights_column'} - a string specifying the name of the column of the
- * input table that gives the weight of each edge. If the graph is to be
- * considered unweighted, then enter 1.</ul>
+ * input table that gives the weight of each edge. </ul>
  *
  * <p> Optional parameter: <ul> <li> {@code orientation} - an integer specifying
  * the orientation of the graph: <ul> <li> 1 if the graph is directed, <li> 2 if
@@ -88,121 +76,20 @@ import org.orbisgis.progress.ProgressMonitor;
  * <p> October 15, 2012: Documentation added by Adam Gouge.
  *
  * @author Erwan Bocher
- * @author Adam Gouge
  */
 public class ST_ShortestPath extends AbstractTableFunction {
 
     /**
-     * The name of this function.
-     */
-    private static final String NAME = "ST_ShortestPath";
-    /**
-     * The number of required arguments.
-     */
-    private static final int NUMBER_OF_REQUIRED_ARGUMENTS = 3;
-    /**
-     * The SQL order of this function.
-     */
-    private static final String SQL_ORDER =
-            "SELECT * from  ST_ShortestPath("
-            + "input_table, "
-            + "source_vertex, "
-            + "target_vertex, "
-            + "'weights_column'"
-            + "[,orientation]);";
-    /**
-     * Short description of this function.
-     */
-    private static final String SHORT_DESCRIPTION =
-            "Calculates the shortest path between two vertices of a graph "
-            + "using Dijkstra's algorithm. ";
-    /**
-     * Long description of this function.
-     */
-    private static final String LONG_DESCRIPTION =
-            "<p> "
-            + "Required parameters: "
-            + "<ul> "
-            + "<li> "
-            + "<code>input_table</code> "
-            + "- the "
-            + "<code>output_table_prefix.edges</code> "
-            + "table produced by the "
-            + "<code>ST_Graph</code> function, except that an extra column "
-            + "must be added to specify the weight of each edge ("
-            + "<code>'weights_column'</code>"
-            + "). "
-            + "<li> "
-            + "<code>source_vertex</code> "
-            + "- specified by an integer. "
-            + "<li> "
-            + "<code>target_vertex</code> "
-            + "- specified by an integer. "
-            + "<li> "
-            + "<code>'weights_column'</code> "
-            + "- a string specifying the name of the column of the input "
-            + "table that gives the weight of each edge. If the graph "
-            + "is to be considered unweighted, then enter 1."
-            + "</ul> " // end required parameters.
-            + "<p> "
-            + "Optional parameter: "
-            + "<code>orientation</code> "
-            + "- an integer specifying the orientation of the graph: "
-            + "<ul> "
-            + "<li> 1 if the graph is directed, "
-            + "<li> 2 if it is directed and we wish to reverse the "
-            + "orientation of the edges, "
-            + "<li> 3 if the graph is undirected. "
-            + "</ul> " // end orientation list
-            + "If no orientation is specified, we assume the graph is"
-            + "directed. "; // end optional parameters. 
-    // REQUIRED ARGUMENTS
-    /**
-     * The source vertex.
-     */
-    private int source;
-    /**
-     * The target vertex.
-     */
-    private int target;
-    /**
-     * Specifies the weight column (or 1 in the case of an unweighted graph).
-     */
-    private String weightsColumn;
-    // OPTIONAL ARGUMENTS
-    /**
-     * Specifies the orientation of the graph.
-     */
-    private int orientation = -1;
-    /**
-     * Description of this function.
-     */
-    private static final String DESCRIPTION =
-            SHORT_DESCRIPTION + LONG_DESCRIPTION;
-    /**
-     * An error message to be displayed when {@link #evaluate(
-     * org.gdms.data.DataSourceFactory,
-     * org.gdms.driver.DataSet[],
-     * org.gdms.data.values.Value[],
-     * org.orbisgis.progress.ProgressMonitor)
-     * fails.
-     */
-    private static final String EVALUATE_ERROR =
-            "Cannot compute the shortest path";
-
-    /**
-     * Evaluates the function to calculate the shortest path using Dijkstra's
+     * Evaluates the function to calculate the shortest path using Dijkstra'
      * algorithm.
      *
-     * @param dsf    The {@link DataSourceFactory} used to parse the data set.
+     * @param dsf The {@link DataSourceFactory} used to parse the data set.
      * @param tables The input table. (This {@link DataSet} array will contain
-     *               only one element since there is only one input table.)
+     * only one element since there is only one input table.)
      * @param values Array containing the optional arguments.
-     * @param pm     The progress monitor used to track the progress of the
-     *               shortest path calculation.
-     *
+     * @param pm The progress monitor used to track the progress of the shortest
+     * path calculation.
      * @return The {@link DataSet} containing the shortest path.
-     *
      * @throws FunctionException
      */
     @Override
@@ -214,114 +101,25 @@ public class ST_ShortestPath extends AbstractTableFunction {
             throws FunctionException {
         try {
             // Set the source vertex.
-            source = GraphFunctionParser.parseSource(values[0]);
+            int source = values[0].getAsInt();
             // Set the target vertex.
-            target = GraphFunctionParser.parseTarget(values[1]);
+            int target = values[1].getAsInt();
             // Set the weights column.
-            weightsColumn = GraphFunctionParser.parseWeight(values[2]);
-
-            parseOptionalArguments(values);
-
-            return calculateShortestPathAccordingToUserInput(
-                    dsf,
-                    tables[0],
-                    pm);
+            String weightsColumn = values[2].getAsString();
+            // If the orientation is specified, we take it into account.
+            if (values.length == 4) {
+                // Calculate and return the shortest path.
+                DiskBufferDriver diskBufferDriver = GraphPath.getShortestPath(dsf, tables[0], source, target, weightsColumn, values[3].getAsInt(), pm);
+                diskBufferDriver.open();
+                return diskBufferDriver;
+            } else { // Orientation is not specified, so we assume the graph is directed.
+                // Calculate and return the shortest path.
+                DiskBufferDriver diskBufferDriver = GraphPath.getShortestPath(dsf, tables[0], source, target, weightsColumn, GraphSchema.DIRECT, pm);
+                diskBufferDriver.open();
+                return diskBufferDriver;
+            }
         } catch (Exception ex) {
-            throw new FunctionException(EVALUATE_ERROR, ex);
-        }
-    }
-
-    /**
-     * Calculates the shortest path according to the SQL arguments provided by
-     * the user.
-     *
-     * @param dsf     The {@link DataSourceFactory} used to parse the data set.
-     * @param dataSet The input table.
-     * @param pm      The progress monitor used to track the progress of the
-     *                calculation.
-     *
-     * @return The shortest path.
-     *
-     * @throws GraphException
-     * @throws DriverException
-     */
-    private DiskBufferDriver calculateShortestPathAccordingToUserInput(
-            DataSourceFactory dsf,
-            DataSet dataSet,
-            ProgressMonitor pm) throws GraphException,
-            DriverException,
-            IndexException {
-
-        DiskBufferDriver diskBufferDriver;
-        // UNWEIGHTED GRAPHS
-        if (weightsColumn == null) {
-            diskBufferDriver =
-                    GraphPathCalculator.calculateShortestPathAllWeightsOne(
-                    dsf,
-                    dataSet,
-                    source,
-                    target,
-                    orientation,
-                    pm);
-        } else { // WEIGHTED GRAPHS
-            diskBufferDriver =
-                    GraphPathCalculator.calculateShortestPath(
-                    dsf,
-                    dataSet,
-                    source,
-                    target,
-                    orientation,
-                    weightsColumn,
-                    pm);
-        }
-        diskBufferDriver.open();
-        return diskBufferDriver;
-    }
-
-    /**
-     * Parse the optional function argument at the given index.
-     *
-     * <p> <i>Note</i>: For this function, there is only one optional argument,
-     * the orientation.
-     *
-     * @param values Array containing the other arguments.
-     * @param index  The index.
-     *
-     * @throws FunctionException
-     */
-    private void parseOptionalArgument(Value[] values, int index) throws
-            FunctionException {
-        orientation = GraphFunctionParser.parseOrientation(values[index]);
-    }
-
-    /**
-     * Parse the optional function arguments.
-     *
-     * <p> <i>Note</i>: If the orientation is not specified, we consider the
-     * graph to be directed.
-     *
-     * @param values Array containing the other arguments.
-     *
-     * @throws FunctionException
-     */
-    private void parseOptionalArguments(Value[] values) throws FunctionException,
-            GraphException {
-        // Set the default orientation to be directed.
-        orientation = GraphSchema.DIRECT;
-        // If the orientation is specified, then recover it.
-        int index = NUMBER_OF_REQUIRED_ARGUMENTS;
-        while (values.length > index) {
-            parseOptionalArgument(values, index++);
-        }
-        System.out.print("Set the orientation to be ");
-        if (orientation == GraphSchema.DIRECT) {
-            System.out.println("directed.");
-        } else if (orientation == GraphSchema.DIRECT_REVERSED) {
-            System.out.println("reversed.");
-        } else if (orientation == GraphSchema.UNDIRECT) {
-            System.out.println("undirected.");
-        } else {
-            throw new GraphException(GraphCreator.GRAPH_TYPE_ERROR);
+            throw new FunctionException("Cannot compute the shortest path", ex);
         }
     }
 
@@ -333,7 +131,7 @@ public class ST_ShortestPath extends AbstractTableFunction {
      */
     @Override
     public String getName() {
-        return NAME;
+        return "ST_ShortestPath";
     }
 
     /**
@@ -343,7 +141,7 @@ public class ST_ShortestPath extends AbstractTableFunction {
      */
     @Override
     public String getSqlOrder() {
-        return SQL_ORDER;
+        return "SELECT * from  ST_ShortestPath(input_table, source_vertex, target_vertex, 'weights_column'[,orientation]);";
     }
 
     /**
@@ -353,7 +151,21 @@ public class ST_ShortestPath extends AbstractTableFunction {
      */
     @Override
     public String getDescription() {
-        return DESCRIPTION;
+        return "Calculates the shortest path between two vertices of a "
+                + "graph using Dijkstra's algorithm. The input_table is the "
+                + "output_table_prefix.edges table produced by the ST_Graph "
+                + "function, except that an extra column must be added to "
+                + "specify the weight of each edge ('weights_column'). The "
+                + "source_vertex and the "
+                + "target_vertex are specified by an integer. The "
+                + "'weights_column' is a string specifying the name of the "
+                + "column of the input table that gives the weight of each "
+                + "edge. The optional parameter orientation is an integer "
+                + "specifying the orientation of the graph: 1 if the graph is "
+                + "directed, 2 if it is directed and we wish to reverse the "
+                + "orientation of the edges, and 3 if the graph is undirected. "
+                + "If no orientation is specified, we assume the graph is "
+                + "directed.";
     }
 
     /**
@@ -361,54 +173,35 @@ public class ST_ShortestPath extends AbstractTableFunction {
      * executing the query.
      *
      * @param tables {@link Metadata} objects of the input tables.
-     *
      * @return The {@link Metadata} of the result.
-     *
      * @throws DriverException
      */
     // TODO: The input 'Metadata[] tables' is never used!
     @Override
     public Metadata getMetadata(Metadata[] tables) throws DriverException {
-        return GraphPathCalculator.createShortestPathMetadata();
+        return GraphMetadataFactory.createEdgeMetadataShortestPath();
     }
 
     /**
      * Returns an array of all possible signatures of this function. Multiple
      * signatures arise from some arguments being optional.
      *
+     * <p> Possible signatures: <OL> <li> {@code (TABLE,INT,INT,STRING)} <li>
+     * {@code (TABLE,INT,INT,STRING,INT)} </OL>
+     *
      * @return An array of all possible signatures of this function.
      */
     @Override
     public FunctionSignature[] getFunctionSignatures() {
         return new FunctionSignature[]{
-                    // ALL WEIGHTS ONE
-                    // First possible signature: (TABLE input_table, INT source, INT target, INT 1).
                     new TableFunctionSignature(
-                    TableDefinition.ANY, // was TableDefinition.GEOMETRY before,
-                    // but we are not necessarily going to return the geometries directly.
-                    new TableArgument(TableDefinition.GEOMETRY),
-                    ScalarArgument.INT,
-                    ScalarArgument.INT,
-                    ScalarArgument.INT),
-                    // Second possible signature: (TABLE input_table, INT source, INT target, INT 1, INT orientation).
-                    new TableFunctionSignature(
-                    TableDefinition.ANY,
-                    new TableArgument(TableDefinition.GEOMETRY),
-                    ScalarArgument.INT,
-                    ScalarArgument.INT,
-                    ScalarArgument.INT,
-                    ScalarArgument.INT),
-                    // WEIGHTED
-                    // First possible signature: (TABLE input_table, INT source, INT target, STRING 'weights_column').
-                    new TableFunctionSignature(
-                    TableDefinition.ANY,
+                    TableDefinition.GEOMETRY,
                     new TableArgument(TableDefinition.GEOMETRY),
                     ScalarArgument.INT,
                     ScalarArgument.INT,
                     ScalarArgument.STRING),
-                    // Second possible signature: (TABLE input_table, INT source, INT target, STRING 'weights_column', INT orientation).
                     new TableFunctionSignature(
-                    TableDefinition.ANY,
+                    TableDefinition.GEOMETRY,
                     new TableArgument(TableDefinition.GEOMETRY),
                     ScalarArgument.INT,
                     ScalarArgument.INT,
