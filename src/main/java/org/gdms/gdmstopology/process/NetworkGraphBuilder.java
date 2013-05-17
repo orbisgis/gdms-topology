e2598cd71a4023f3e7225bcf7e7ff408d003716c
Adam Gouge <adam.gouge@ec-nantes.fr>
2013-05-17 09:46:15 +0200

    Add method for creating edge metadata

--- src/main/java/org/gdms/gdmstopology/process/NetworkGraphBuilder.java
+++ src/main/java/org/gdms/gdmstopology/process/NetworkGraphBuilder.java
@@ -42,6 +42,7 @@ import org.gdms.data.DataSourceFactory;
 import org.gdms.data.NonEditableDataSourceException;
 import org.gdms.data.indexes.rtree.DiskRTree;
 import org.gdms.data.schema.DefaultMetadata;
+import org.gdms.data.schema.Metadata;
 import org.gdms.data.schema.MetadataUtilities;
 import org.gdms.data.types.Type;
 import org.gdms.data.types.TypeFactory;
@@ -165,21 +166,13 @@ public class NetworkGraphBuilder {
             DiskRTree diskRTree = new DiskRTree();
             diskRTree.newIndex(new File(diskTreePath));
 
-            // Obtain the original metadata from the input table to which
-            // we will append the metadata for the edges.
-            DefaultMetadata edgeMedata =
-                    new DefaultMetadata(dataSet.getMetadata());
+            // Obtain the original metadata from the input table
+            Metadata originalMD = dataSet.getMetadata();
             // Count the number of fields in the input table.
-            int srcFieldsCount = edgeMedata.getFieldCount();
-
-            // Add fields to the metadata for the id, start_node, and end_node.
-            edgeMedata.addField(GraphSchema.ID,
-                                TypeFactory.createType(Type.INT));
-            edgeMedata.addField(GraphSchema.START_NODE,
-                                TypeFactory.createType(Type.INT));
-            edgeMedata.addField(GraphSchema.END_NODE,
-                                TypeFactory.createType(Type.INT));
-
+            int srcFieldsCount = originalMD.getFieldCount();
+            
+            // Create the edge metadata.
+            DefaultMetadata edgeMedata = createEdgeMetadata(originalMD);
             // Count the number of fields in the edge metadata.
             int fieldsCount = edgeMedata.getFieldCount();
 
@@ -312,5 +305,29 @@ public class NetworkGraphBuilder {
         //Remove the Rtree on disk
         new File(diskTreePath).delete();
         pm.endTask();
+    }
+
+    /**
+     * Create the edge metadata by appending id, start node and end node columns
+     * to the original metadata.
+     *
+     * @param originalMD Original metadata
+     *
+     * @return Metadata for edges
+     *
+     * @throws DriverException
+     */
+    private DefaultMetadata createEdgeMetadata(Metadata originalMD) throws
+            DriverException {
+        // Make a copy of the original metadata.
+        DefaultMetadata edgeMedata = new DefaultMetadata(originalMD);
+        // Add fields to the metadata for the id, start_node, and end_node.
+        edgeMedata.addField(GraphSchema.ID,
+                            TypeFactory.createType(Type.INT));
+        edgeMedata.addField(GraphSchema.START_NODE,
+                            TypeFactory.createType(Type.INT));
+        edgeMedata.addField(GraphSchema.END_NODE,
+                            TypeFactory.createType(Type.INT));
+        return edgeMedata;
     }
 }
